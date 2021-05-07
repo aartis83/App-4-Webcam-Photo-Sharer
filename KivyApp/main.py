3a83f984d59a47d75d39fce02d79268e49285863
Aarti Srinivasan <aartisrinivasan83@gmail.com>
2021-05-07 16:33:04 -0700

    Add files via upload

---
+++ KivyApp/main.py
@@ -0,0 +1,44 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+
+import wikipedia
+import requests
+
+Builder.load_file('front.kv')
+
+
+class FirstScreen(Screen):
+
+    def get_image_link(self):
+        # Get user query from text input
+        query = self.manager.current_screen.ids.user_query.text
+        # Get wikipedia page and first image link
+        page = wikipedia.page(query)
+        image_link = page.images[0]
+        return image_link
+
+    def download_image(self):
+        # Download the image
+        req = requests.get(self.get_image_link())
+        imagepath = 'images/image.jpg'
+        with open(imagepath, 'wb') as file:
+            file.write(req.content)
+        return imagepath
+
+    def set_image(self):
+        # Set the image in Image widget
+        self.manager.current_screen.ids.img.source = self.download_image()
+
+
+class RootWidget(ScreenManager):
+    pass
+
+
+class MainApp(App):
+
+    def build(self):
+        return RootWidget()
+
+
+MainApp().run()
